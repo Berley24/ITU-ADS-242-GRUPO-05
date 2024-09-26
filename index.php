57d115697232bcb8e9623f40387c02cd0118b2a0
Berley24 <158833529+Berley24@users.noreply.github.com>
2024-09-26 01:30:49 -0300

    Add files via upload
    Signed-off-by: Berley24 <158833529+Berley24@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Novo Projeto</title>
+</head>
+
+<body>
+    <header class="menu-principal">
+        <main>
+            <div class="header-1">
+                <div class="logo">
+                    <!--<img src="img/logo.png">-->
+                    <h2><?php echo 'Coleira Tec'; ?></h2>
+                </div>
+                <div class="redes-sociais">
+                    <ul>
+                        <li><a href=""><img src="img/rss.png"></a></li>
+                        <li><a href=""><img src="img/face.png"></a></li>
+                        <li><a href=""><img src="img/tw.png"></a></li>
+                        <li><a href=""><img src="img/linkedin.png"></a></li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+    </header>
+
+    <main class="col-100 menu-urls">
+        <div class="header-2">
+            <div class="menu">
+                <ul>
+                    <li><a href=""><?php echo 'Home'; ?></a></li>
+                    <li><a href=""><?php echo 'About us'; ?></a></li>
+                    <li><a href=""><?php echo 'Service'; ?></a></li>
+                    <li><a href=""><?php echo 'Page'; ?></a></li>
+                    <li><a href=""><?php echo 'Blog'; ?></a></li>
+                    <li><a href=""><?php echo 'Contact Us'; ?></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="busca">
+            <input placeholder="Search Something" type="text">
+        </div>
+    </main>
+
+    <div class="col-100">
+        <div class="slider-principal">
+            <img src="./img/ca7.jpeg">
+            <img src="./img/ca3.jpeg">
+            <img src="./img/ca4.jpeg">
+            <img src="./img/ca6.jpeg">
+        </div>
+    </div>
+
+    <div class="col-100">
+        <div class="content texto-destaque">
+            <h1><?php echo 'sabe onde está <strong>o seu cachorro</strong> com facilidade.'; ?></h1>
+            <p><?php echo 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.'; ?></p>
+
+            <div class="col-3 bloco-texto">
+                <img src="img/content-1.png">
+                <h3><?php echo 'Lorem Ipsum is simply'; ?></h3>
+                <p><?php echo 'Lorem Ipsum is simply dummy text of the printing and typesetting of the industry. Lorem Ipsum has been'; ?></p>
+            </div>
+            <div class="col-3 bloco-texto">
+                <img src="img/content-2.png">
+                <h3><?php echo 'Lorem Ipsum is simply'; ?></h3>
+                <p><?php echo 'Lorem Ipsum is simply dummy text of the printing and typesetting of the industry. Lorem Ipsum has been'; ?></p>
+            </div>
+            <div class="col-3 bloco-texto">
+                <img src="img/content-3.png">
+                <h3><?php echo 'Lorem Ipsum is simply'; ?></h3>
+                <p><?php echo 'Lorem Ipsum is simply dummy text of the printing and typesetting of the industry. Lorem Ipsum has been'; ?></p>
+            </div>
+        </div>
+    </div>
+
+    </script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js">
+    </script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+
+</body>
+
+</html>
